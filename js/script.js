e90f5e1e533b822d1119d5e2f46cbec33d7bac09
Rosseau Nilo Maamor <100171901+RssnlGM@users.noreply.github.com>
2023-05-01 01:03:50 +0800

    Update and rename script.js to js/script.js
    Added JS for New login page 05/01/2023

--- js/script.js
+++ js/script.js
@@ -39,3 +39,24 @@ function view(){
 function search_back(){
     document.location = 'search.php';
 }
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
